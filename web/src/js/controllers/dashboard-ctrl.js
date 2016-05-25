ae9b264a6b6981fb1ce5ff34a1d898cb96a0990c
Jan Matas <jm6214@ic.ac.uk>
2016-05-25 16:45:32 +0100

    Added the number of devices in one of the top widgets.
    Signed-off-by: Jan Matas <jm6214@ic.ac.uk>

--- web/src/js/controllers/dashboard-ctrl.js
+++ web/src/js/controllers/dashboard-ctrl.js
@@ -1,10 +1,17 @@
 /**
  * Dashboard Controller
  */
+
 angular
     .module('Persica')
-    .controller('DashboardCtrl', ['$scope', DashboardCtrl]);
+    .controller('DashboardCtrl', ['$scope', 'deviceService', DashboardCtrl]);
 
-function DashboardCtrl($scope) {
+function DashboardCtrl($scope, deviceService, $uibModal) {
 
+	var devices_observer = function() {
+		$scope.deviceCount = Object.keys(deviceService.devices).length;
+	}
+
+    deviceService.observers.push(devices_observer)
 }
+
