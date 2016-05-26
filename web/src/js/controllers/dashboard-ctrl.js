4f269652830d0a5067457b0faa85aa64e763ea13
Paul Balaji <pmb14@ic.ac.uk>
2016-05-26 14:18:54 +0100

    [PB] implementing chat service frontend
    Signed-off-by: Paul Balaji <pmb14@ic.ac.uk>

--- web/src/js/controllers/dashboard-ctrl.js
+++ web/src/js/controllers/dashboard-ctrl.js
@@ -4,14 +4,18 @@
 
 angular
     .module('Persica')
-    .controller('DashboardCtrl', ['$scope', 'deviceService', DashboardCtrl]);
+    .controller('DashboardCtrl', ['$scope', 'deviceService', 'chatService', DashboardCtrl]);
 
-function DashboardCtrl($scope, deviceService, $uibModal) {
+function DashboardCtrl($scope, deviceService, chatService, $uibModal) {
 
 	var devices_observer = function() {
 		$scope.deviceCount = Object.keys(deviceService.devices).length;
 	};
-
     deviceService.observers.push(devices_observer);
+
+    var chat_observer = function() {
+        $scope.messages = chatService.messages;
+    };
+    chatService.observers.push(chat_observer);
 }
 
