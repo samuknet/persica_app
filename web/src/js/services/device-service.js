7fe318b3ee35a9ecb74b3a47f492891fc7855092
Paul Balaji <pmb14@ic.ac.uk>
2016-05-26 17:19:19 +0100

    [PB][GP] log timing working better AND moved entire timing log + pie chart to devices page, TODO: adding persistency and links with DB.
    Signed-off-by: Paul Balaji <pmb14@ic.ac.uk>

--- web/src/js/services/device-service.js
+++ web/src/js/services/device-service.js
@@ -23,6 +23,7 @@ app.service('deviceService', ['$http', 'socketService', function ($http, socketS
 	socketService.on('device-connected', function(device) {
 		devices[device.did] = _.extend(devices[device.did] || {}, device);
 		devices[device.did].online = true;
+        devices[device.did].establishTime = Date.now();
 		notify_observers();
 
     });
@@ -30,6 +31,7 @@ app.service('deviceService', ['$http', 'socketService', function ($http, socketS
     socketService.on('device-disconnected', function(device) {
 		devices[device.did] = _.extend(devices[device.did] || {}, device);
 		devices[device.did].online = false;
+        devices[device.did].lastOnline = Date.now()
 		notify_observers();
 
         
