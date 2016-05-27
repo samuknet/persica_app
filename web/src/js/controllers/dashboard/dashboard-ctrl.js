546b91ce7c5973b8664cf4d55117ec526054d9ae
Sam Wood <sam@sam.uk.net>
2016-05-27 10:12:09 +0100

    [SW] Factored out dashboard wigets into separate template files for readability.
    Signed-off-by: Sam Wood <sam@sam.uk.net>

--- web/src/js/controllers/dashboard/dashboard-ctrl.js
+++ web/src/js/controllers/dashboard/dashboard-ctrl.js
@@ -7,10 +7,5 @@ angular
     .controller('DashboardCtrl', ['$scope', 'deviceService', DashboardCtrl]);
 
 function DashboardCtrl($scope, deviceService) {
-    var devices_observer = function() {
-        $scope.deviceCount = Object.keys(deviceService.devices).length;
-    };
 
-    deviceService.observers.push(devices_observer);
-    devices_observer();
 }
---
+++ web/src/js/controllers/dashboard/dashboardHeader-ctrl.js
@@ -0,0 +1,13 @@
+angular
+    .module('Persica')
+    .controller('DashboardHeaderCtrl', ['$scope', 'deviceService', DashboardHeaderCtrl]);
+
+function DashboardHeaderCtrl($scope, deviceService) {
+	var devices_observer = function() {
+        $scope.deviceCount = Object.keys(deviceService.devices).length;
+    };
+
+    deviceService.observers.push(devices_observer);
+    devices_observer();
+
+}
