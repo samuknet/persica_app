71917b74127ccfa317e8ab25dfa591ecce2346e4
Sam Wood <sam@sam.uk.net>
2016-06-03 12:51:36 +0100

    [SW] Updated db config to use local mongo instance
    Signed-off-by: Sam Wood <sam@sam.uk.net>

--- config/db.js
+++ config/db.js
@@ -1,3 +1,4 @@
 module.exports = {
-    url : 'mongodb://admin:password@ds011482.mlab.com:11482/heroku_dd7j0sxs'
+    // url : 'mongodb://admin:password@ds011482.mlab.com:11482/heroku_dd7j0sxs'
+    url: 'mongodb://127.0.0.1:27017/'
 }
