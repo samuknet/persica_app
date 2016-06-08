628d696717fddd0ed5c004616d57fec19c455cd6
Paul Balaji <pmb14@ic.ac.uk>
2016-06-08 14:19:58 +0100

    [PB] updated user DB schema to now support customisable notifications
    Signed-off-by: Paul Balaji <pmb14@ic.ac.uk>

--- app/models/user.js
+++ app/models/user.js
@@ -6,6 +6,7 @@ var UserSchema = new mongoose.Schema({
   username: {type: String, lowercase: true, unique: true},
   hash: String,
   salt: String,
+  notifyConfig: [{type: Number, dst: String}]
 });
 
 UserSchema.methods.setPassword = function(password){
