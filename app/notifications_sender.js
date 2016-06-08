01fda1568ba8f912e171da33d1a51710d9b1c668
Galia Peycheva <galina.peycheva14@imperial.ac.uk>
2016-06-08 16:33:55 +0100

    [GP] Email sending implemented.

---
+++ app/notifications_sender.js
@@ -0,0 +1,39 @@
+var nodemailer = require('nodemailer');
+
+var smtpTransport = nodemailer.createTransport("SMTP",{
+   service: "Gmail",  // sets automatically host, port and connection security settings
+   auth: {
+       user: "io.persica@gmail.com",
+       pass: "persica2016"
+   }
+});
+
+var exports = {}
+
+exports.sendEmail = function(email, logObj) {
+	var mailOptions = {
+   		from: '"Persica 👥" <notifications@persica.io>',
+   		to: email,
+   		subject: "Critical Level " +  logObj.critical + " reached on device " + logObj.did,
+    	text: logObj.log
+    };
+
+    smtpTransport.sendMail(mailOptions, function(error, info) {
+    if(error){
+       console.log(error);
+    }else{
+       console.log("Message sent: " + info.message);
+    }
+   
+    smtpTransport.close();
+});
+
+};
+
+exports.sendSMS = function(phoneNumber, logObj) {
+
+};
+
+module.exports = exports 
+
+
---
+++ app/notifications_test.js
@@ -0,0 +1,3 @@
+var ns = require('./notifications_sender.js');
+
+ns.sendEmail("kookoogirl@abv.bg", {did:4, log:"test3"});
