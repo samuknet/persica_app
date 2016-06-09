2fe13b99132400c7fb026520ad257e352ae127e5
Jan Matas <jm6214@ic.ac.uk>
2016-06-09 12:47:15 +0100

    [JM] Minor changes to Group Controller

--- web/src/js/controllers/group/groupProfile-ctrl.js
+++ web/src/js/controllers/group/groupProfile-ctrl.js
@@ -3,33 +3,16 @@
  */
 angular
     .module('Persica')
-    .controller('GroupProfileCtrl', ['$scope', '$stateParams', '$http', 'deviceService', GroupProfileCtrl]);
+    .controller('GroupProfileCtrl', ['$scope', '$stateParams', '$http', 'groupService', GroupProfileCtrl]);
 
-function GroupProfileCtrl($scope, $stateParams, $http, deviceService) {
-    var did = $stateParams.did;
+function GroupProfileCtrl($scope, $stateParams, $http, groupService) {
+    var gid = $stateParams.gid;
     var device_observer = function() {
-        $scope.device = deviceService.devices[did];
+        $scope.device = groupService.groups[gid];
     };
-
-    deviceService.observers.push(device_observer);
+    groupService.observers.push(device_observer);
     device_observer();
 
-    $scope.labels = ["22nd January 2016", "15th March 2016", "7th April 2016", "ello", "sd", "asd"];
-    $scope.upTime = [50 , 20, 60, 20, 10, 40];
-    $scope.uptimePairs = [[100, 150], [180, 300], [350, 410]];
-    $scope.averageUpTime = calcAverageUpTime($scope.uptimePairs);
-
-    function calcAverageUpTime(uptimePairs) {
-        var sum = 0
-        for (i = 0; i < uptimePairs.length; i++) {
-            var startTime = uptimePairs[i][0];
-            var endTime = uptimePairs[i][1];
-            var upTime = endTime - startTime;
-            sum = sum + upTime;
-        }
-        var averageUpTime = sum / uptimePairs.length;
-        return averageUpTime;
-    }
 }
 
 angular
@@ -117,7 +100,6 @@ angular.module('Persica').controller('DeviceVariableGraphModalCtrl', ['$scope', 
     }
 }]);
 
-/* Controller for the graph modal for a device variable */
 angular.module('Persica').controller('DeviceLogsCtrl', ['$scope', '$stateParams', 'deviceService', function($scope, $stateParams, deviceService) {
     var did = $stateParams.did;
     $scope.test = '';
